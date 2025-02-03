b26c714c5b836a87f4f4b8cbc1c5b7c55ab27b79
BIT <>
2025-02-03 16:52:40 +0900

    frontend #7 02/03

--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -1,3 +1,23 @@
 console.log("============[02.02] 표현식(Expression)");
+
+2+2 // 개행 있으면 실행
+n=4
+parseInt('4')
+Math.pow(2,2)
+4
+
 console.log("============[05] 세미콜론(Semicolon)");
+
+a=10; b=20;
+
 console.log("============[06] 개행(Line Break)");
+
+i
+=
+10
+console.log(i)
+
+/*
+console.lo
+g('hello world') => console 객체의 lo 속성이 undefined라고 인식되므로 에러
+*/
